5aa10c4eaf55a359ed5d8c687e73b2ba8c3b584a
unknown <jam319@protonmail.com>
2022-05-14 23:59:40 -0400

    Added the edit form's functionality

--- controllers/places.js
+++ controllers/places.js
@@ -37,16 +37,30 @@ router.get('/:id', (req, res) => {
   }
 })
 
-router.get('/:id/edit', (req, res) => {
+router.put('/:id', (req, res) => {
   let id = Number(req.params.id)
   if (isNaN(id)) {
-    res.render('error404')
+      res.render('error404')
   }
   else if (!places[id]) {
-    res.render('error404')
+      res.render('error404')
   }
   else {
-  res.render('places/edit', { place: places[id] })
+      // Dig into req.body and make sure data is valid
+      if (!req.body.pic) {
+          // Default image if one is not provided
+          req.body.pic = 'http://placekitten.com/400/400'
+      }
+      if (!req.body.city) {
+          req.body.city = 'Anytown'
+      }
+      if (!req.body.state) {
+          req.body.state = 'USA'
+      }
+
+      // Save the new data into places[id]
+      places[id] = req.body
+      res.redirect(`/places/${id}`)
   }
 })
 
@@ -61,6 +75,19 @@ router.delete('/:id', (req, res) => {
   else {
     places.splice (id, 1)
     res.redirect('/places')
+  }
+})
+
+router.get('/:id/edit', (req, res) => {
+  let id = Number(req.params.id)
+  if (isNaN(id)) {
+    res.render('error404')
+  }
+  else if (!places[id]) {
+    res.render('error404')
+  }
+  else {
+  res.render('places/edit', { place: places[id], id })
   }
 })
 
--- views/places/edit.jsx
+++ views/places/edit.jsx
@@ -13,13 +13,15 @@ function edit_form (data) {
                         <input className="form-control" 
                                 id="name" 
                                 name="name" 
+                                value={data.place.name}
                                 required />
                     </div>
                     <div className="form-group col-sm-6">
                         <label htmlFor="pic">Place Picture</label>
                         <input className="form-control" 
                                 id="pic" 
-                                 name="pic" />
+                                 name="pic"
+                                 value={data.place.pic} />
                     </div>
                 </div>
                 <div className="row">
@@ -27,20 +29,23 @@ function edit_form (data) {
                         <label htmlFor="city">City</label>
                         <input className="form-control" 
                                 id="city" 
-                                name="city" />
+                                name="city"
+                                value={data.place.city} />
                     </div>
                     <div className="form-group col-sm-6">
                         <label htmlFor="state">State</label>
                         <input className="form-control" 
                                 id="state" 
-                                name="state" />
+                                name="state"
+                                value={data.place.state} />
                     </div>
                 </div>
                     <div className="form-group">
                         <label htmlFor="cuisines">Cuisines*</label>
                         <input className="form-control" 
                                 id="cuisines" 
-                                name="cuisines" 
+                                name="cuisines"
+                                value={data.place.cuisines} 
                                 required />
                     </div>
                     <input className="btn btn-primary" type="submit" value="Edit Place" />
