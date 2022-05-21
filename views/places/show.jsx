91736a4601f56f2396db76153f937dfc5a2d1e64
unknown <jam319@protonmail.com>
2022-05-20 20:12:54 -0400

    added showEstablished helper method

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -22,6 +22,9 @@ function show (data) {
                   <h2>
                     Description
                   </h2>
+                  <h3>
+                    {data.place.showEstablished()}
+                  </h3>
                   <h4>
                     Serving {data.place.cuisines}
                   </h4>
