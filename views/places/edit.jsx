4107a9244c5729e1e66fccdf5e5922291cffa49e
unknown <jam319@protonmail.com>
2022-05-30 06:04:53 -0400

    added rating system and the ability to delete comments

--- views/places/edit.jsx
+++ views/places/edit.jsx
@@ -15,7 +15,7 @@ function edit_form (data) {
                                 name="name" 
                                 value={data.place.name}
                                 required
-                                />
+                        />
                     </div>
                     <div className="form-group col-sm-6">
                         <label htmlFor="pic">Place Picture</label>
@@ -23,25 +23,34 @@ function edit_form (data) {
                                 id="pic" 
                                  name="pic"
                                  value={data.place.pic}
-                                 />
+                        />
                     </div>
                 </div>
                 <div className="row">
-                    <div className="form-group col-sm-6">
+                    <div className="form-group col-sm-4">
                         <label htmlFor="city">City</label>
                         <input className="form-control" 
                                 id="city" 
                                 name="city"
                                 value={data.place.city}
-                                />
+                        />
                     </div>
-                    <div className="form-group col-sm-6">
+                    <div className="form-group col-sm-4">
                         <label htmlFor="state">State</label>
                         <input className="form-control" 
                                 id="state" 
                                 name="state"
                                 value={data.place.state}
-                                />
+                        />
+                    </div>
+                    <div className="form-group col-sm-4">
+                        <label htmlFor="founded">Founding Year</label>
+                        <input className="form-control" 
+                               id="founded" 
+                               name="founded" 
+                               value={data.place.founded} 
+                        />
+                    
                     </div>
                 </div>
                     <div className="form-group">
